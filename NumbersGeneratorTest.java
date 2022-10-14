5cbea74d72d81fb8d0b3ec30ae0113f4388f56be
Jacek <steiner@10g.pl>
2022-10-14 14:06:33 +0200

    test inside

--- NumbersGeneratorTest.java
+++ NumbersGeneratorTest.java
@@ -1,15 +1,12 @@
 package games.parts;
 
-import games.parts.LotteryParameters;
-import games.parts.NumbersGenerator;
 import org.junit.jupiter.api.Assertions;
 
 class NumbersGeneratorTest {
-
     @org.junit.jupiter.api.Test
     void generateNumbers() {
-       NumbersGenerator testLuckyNumbers = new NumbersGenerator();
+        NumbersGenerator testLuckyNumbers = new NumbersGenerator();
         Assertions.assertEquals(false, testLuckyNumbers.generateNumbers().isEmpty());
-        Assertions.assertEquals(LotteryParameters.lotterySize,testLuckyNumbers.generateNumbers().size());
+        Assertions.assertEquals(LotteryParameters.lotterySize, testLuckyNumbers.generateNumbers().size());
     }
 }
