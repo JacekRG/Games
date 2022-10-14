dbddc81b569b956e6e23add12fb3481ae6518a60
Jacek <steiner@10g.pl>
2022-10-14 11:28:58 +0200

    with new class

--- NumbersGenerator.java
+++ NumbersGenerator.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 
 public class NumbersGenerator {
     HashSet<Integer> luckyNumbers = new HashSet<>();
-
     public HashSet<Integer> generateNumbers() {
         while (luckyNumbers.size() < LotteryParameters.lotterySize) {
             luckyNumbers.add((int) (Math.floor(Math.random() * LotteryParameters.numbersRange) + 1));
